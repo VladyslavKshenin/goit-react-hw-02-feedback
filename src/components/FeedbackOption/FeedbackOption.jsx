cdcffafae15c275640675d568ee849fd64e193f4
VladyslavKshenin <legendary.1bk@gmail.com>
2024-01-05 16:13:54 +0200

    add style

--- src/components/FeedbackOption/FeedbackOption.jsx
+++ src/components/FeedbackOption/FeedbackOption.jsx
@@ -1,17 +1,18 @@
 import { List } from "./FeedbackOption.stayled"
+import { Button} from 'antd';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return (
         <List>
             {options.map((option, index) => (
-                    <li key={ index } >
-                    <button
-                        type="button"
+                <li key={index} >
+                    <Button
+                        type="primary"
                         onClick={onLeaveFeedback}
                         name={option}
                     >
                         {option}
-                    </button>
+                    </Button>
                 </li>
             ))}
         </List>
