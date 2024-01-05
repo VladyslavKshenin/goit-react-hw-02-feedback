037f76deff3d67019eb2f6451f3e74415d59c37e
VladyslavKshenin <legendary.1bk@gmail.com>
2024-01-05 16:18:28 +0200

    remade

--- src/components/FeedbackOption/FeedbackOption.jsx
+++ src/components/FeedbackOption/FeedbackOption.jsx
@@ -1,18 +1,17 @@
 import { List } from "./FeedbackOption.stayled"
-import { Button} from 'antd';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return (
         <List>
             {options.map((option, index) => (
                 <li key={index} >
-                    <Button
-                        type="primary"
+                    <button
+                        type="button"
                         onClick={onLeaveFeedback}
                         name={option}
                     >
                         {option}
-                    </Button>
+                    </button>
                 </li>
             ))}
         </List>
--- src/components/FeedbackOption/FeedbackOption.stayled.js
+++ src/components/FeedbackOption/FeedbackOption.stayled.js
@@ -10,22 +10,22 @@ export const List = styled('ul') (() => {
     button: {
         width: "160px",
         height:"60px",
-    //     backgroundColor: "rgb(255, 251, 0)",
-    //     border: "none",
-    //     padding: "10px",
-    //     borderRadius: "8px",
+        backgroundColor: "rgb(255, 251, 0)",
+        border: "none",
+        padding: "10px",
+        borderRadius: "8px",
         textTransform: "capitalize",
         fontSize: "24px",
-    //     color: "#4f4fc2",
-    //     boxShadow: " 0 15px 15px rgba(124, 97, 245, 0.3)",
-    //     letterSpacing: "1px",
-    //     transition: "all 500",
-    //     '&:hover': {
-    //         letterSpacing: "3px",
-    //         background: "#7b7bf1",
-    //         boxShadow: "0 0 5px #4f4fc2, 0 0 15px #4f4fc2, 0 0 30px #4f4fc2, 0 0 60px #4f4fc2",
-    //         color:"white"
-    //    }
+        color: "#4f4fc2",
+        boxShadow: " 0 15px 15px rgba(124, 97, 245, 0.3)",
+        letterSpacing: "1px",
+        transition: "all 500",
+        '&:hover': {
+            letterSpacing: "3px",
+            background: "#7b7bf1",
+            boxShadow: "0 0 5px #4f4fc2, 0 0 15px #4f4fc2, 0 0 30px #4f4fc2, 0 0 60px #4f4fc2",
+            color:"white"
+       }
         }
             
     }
